d1b07d8070ac2bbef2026f89beb334ff88bd804d
KazaKazan <ahmetfiratusta@hotmail.com>
2021-11-26 15:19:00 +0300

    Add comments to script.js

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const player = () => {
 };
 
 const gameBoard = (() => {
-
+        //The gameboard consists of three arrays representing rows, each "row" consisting of three empty
+        //strings which represent individual tiles on a given "column". An empty string represents an empty
+        //tile, a tile claimed by Player 1 is indicated by "X" and one claimed by Player 2 is indicated by "O"
         let tiles = [["","",""],["","",""],["","",""]];
 
+        //An array of all possible 3-tile long lines where a match can occur.
         const lines = [
             //Horizontal Lines
             tiles[0],tiles[1],tiles[2],
@@ -34,13 +37,17 @@ const gameBoard = (() => {
 
 const gameLogic = (() => {
 
+    //Checks every tile in a given line, returns true if they're all the same value to indicate a match,
+    //otherwise returns false.
     const _checkTiles = function (line) {
         return line.every((tile) => {
             return tile === line[0];
         });
     };
 
-    const _checkMatch = function (lines) {
+    //Checks the whole board for matches, returns true if a match is present on the board,
+    //otherwise returns false.
+    const _checkBoard = function (lines) {
         let matchFound = false;
 
         for(let line of lines){
